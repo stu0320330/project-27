e2e965c402c19ce3664aaf0bc1f07898ef7030d4
Savinu Nugith Dulwan Wimalasuriya <150595620+stu0320330@users.noreply.github.com>
2023-11-13 16:59:28 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(steps)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.Shake, function () {
+    steps += 1
+})
+let steps = 0
+steps = 0
+basic.forever(function () {
+	
+})
